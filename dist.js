08067c600258c973c308aee283d5a8902966483a
John O'Sullivan <john@eximchain.com>
2019-02-08 15:47:20 -0500

    dist update

--- dist.js
+++ dist.js
@@ -18,7 +18,8 @@ var valOrNull = function valOrNull(obj, name) {
 var mainNetTxExecutors = ['http://ec2-3-80-165-200.compute-1.amazonaws.com:8080', 'http://ec2-34-219-140-122.us-west-2.compute.amazonaws.com:8080', 'http://ec2-54-255-248-144.ap-southeast-1.compute.amazonaws.com:8080'];
 var gammaTxExecutor = 'http://ec2-3-91-44-3.compute-1.amazonaws.com:8080';
 
-var checkEXC = function checkEXC(address, opts) {
+var checkEXC = function checkEXC(address) {
+  var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
   var web3 = new Web3(new Web3.providers.HttpProvider(opts.useTestNet ? gammaTxExecutor : sample(mainNetTxExecutors), valOrNull(opts, 'timeout'), valOrNull(opts, 'user'), valOrNull(opts, 'password'), valOrNull(opts, 'headers')));
   return web3.eth.getBalance(opts.requireChecksum ? address : address.toLowerCase());
 };
